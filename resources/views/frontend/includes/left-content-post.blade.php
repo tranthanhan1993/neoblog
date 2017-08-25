a6cf468183a603c619f37f846b0a49ce78fccce1
an tran <antran.pk@gmail.com>
2017-08-25 18:15:15 +0700

    last commit

--- resources/views/frontend/includes/left-content-post.blade.php
+++ resources/views/frontend/includes/left-content-post.blade.php
@@ -40,7 +40,7 @@
      @foreach ($comments as $comment)
     <div class="media">
         <a class="pull-left" href="#">
-            <img class="media-object" src="http://placehold.it/64x64" alt="">
+            <img class="media-object" src="{{$comment->user->getAvatarPath()}}" alt="">
         </a>
         <div class="media-body">
             <h4 class="media-heading">{{$comment->user->name}}</h4>
